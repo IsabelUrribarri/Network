8738fa54d996a29fc687a891eb760567b19820f1
IsabelUrribarri <urribarriisabel@gmail.com>
2022-11-14 09:58:00 -0400

    index html new post form

--- network/models.py
+++ network/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    text = models.TextField(blank = True)
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posted_by")
+
+    def __str__(self):
+        return f"{self.id}: {self.text}"
