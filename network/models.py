f32c1fb092ea4efdde01f9905a288cb173306871
IsabelUrribarri <urribarriisabel@gmail.com>
2022-11-14 10:08:10 -0400

    like and comment models

--- network/models.py
+++ network/models.py
@@ -13,3 +13,22 @@ class Post(models.Model):
 
     def __str__(self):
         return f"{self.id}: {self.text}"
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_likes")
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.user.username} - {self.post.text}" 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post_comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_comments")
+    comment = models.TextField()
+    date_creation = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.user.username} - {self.comment} - {self.post.text}" 
